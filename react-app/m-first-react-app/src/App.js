f50e925a8e56c478d9bfaff9fb3156fd905e218f
kansg92 <kansg922@gmail.com>
2023-05-13 15:39:26 +0900

    react-Router

--- react-app/m-first-react-app/src/App.js
+++ react-app/m-first-react-app/src/App.js
@@ -1,36 +1,19 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [money, setMoney] = useState(0);
-  const [coins, setCoins] = useState([]);
-  const onChange = (event) => setMoney(event.target.value);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <input type="number" value={money} onChange={onChange}/>
-      <hr />
-      <h3>you Colud buy</h3>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): {Math.round((money / coin.quotes.USD.price)*100)/100}ea
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route>
+          <Route path="movie" element={<Detail />} />
+        </Route>
+        <Route>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
