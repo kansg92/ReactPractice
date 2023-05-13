e69a2e8098538212d0c409cfbd9612d2188e22a2
kansg92 <kansg922@gmail.com>
2023-05-13 11:15:22 +0900

    useEffect

--- react-app/m-first-react-app/src/App.js
+++ react-app/m-first-react-app/src/App.js
@@ -1,11 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  const onChange = (event) => setKeyword(event.target.value);
+  //console.log("i run all the time");
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(()=>{
+    console.log("I run when 'keyword' chages.");
+  }, [keyword]);
+  useEffect(()=>{
+    console.log("I run when 'counter' chages.");
+  }, [counter]);
+  useEffect(()=>{
+    console.log("I run when 'keyword' & 'counter' chages.")
+  }, [keyword, counter])
   return (
-    <div className="App">
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
